b577414a3a7ffedbc9139fe44bc0d8586bd432dd
el-sam <samurai.h@yahoo.com>
2023-09-17 21:54:32 +0800

    define function to update the propety non-destructively

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// Define an array for all employees
+const employee = {
+    name: "Alex",
+    streetAddress: { 
+        number: 15,
+        stree: "Kudos Road",
+        city: "Byford",
+        postcode: 1124
+    }   
+    ,
+};
+
+function updateEmployeeWithKeyAndValue(employee,key, value){
+    // Create a copy of the original object
+    const newEmployee = {...employee};
+    newEmployee[key] = value; 
+    return newEmployee;
+}
+
+
+
